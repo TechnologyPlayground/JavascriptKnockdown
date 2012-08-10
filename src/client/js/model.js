ef8cde02c8767a781a4569e14f09caa1ad5a88e8
Andrew Benz <andorbal@gmail.com>
2012-08-09 21:49:27 -0500

    Fleshed out the game dealing logic.

--- src/client/js/model.js
+++ src/client/js/model.js
@@ -3,6 +3,7 @@ function game () {
 
   this.currentRound = {};
   this.deck = [];
+  this.currentCard = 0;
 
   this.suits = ["D", "C", "H", "S"];
   this.numbers = ["J", "Q", "K", "A"];
@@ -31,9 +32,18 @@ function game () {
   };
 
   // Create the current round
-  this.deal = function() {};
+  this.deal = function() {
+    self.currentRound = new round(self);
+  };
 
-  //this.shuffle();
+  this.nextCard = function() {
+    if (self.currentCard > 51) {
+      return null;
+    }
+
+    return self.deck[self.currentCard++];
+  };
+
   for (var i = 2; i < 11; i++) {
     this.numbers.push(i);
   }
@@ -43,19 +53,31 @@ function game () {
       self.deckOfCards.push(self.suits[suitIndex] + self.numbers[numberIndex]);
     }
   }
+  this.shuffle();
 }
 
-function round(currentDeck) {
-  this.player = {};
-  this.dealer = {};
+function round(currentGameObj) {
+  var self = this;
+  this.currentGame = currentGameObj;
+
+  this.player = new hand();
+  this.dealer = new hand();
+
+  this.players = [this.player, this.dealer];
+
   this.currentPlayer = {};
   this.hit = function() {};
   this.stand = function() {};
+
+  for (var i = 0; i < this.players.length * 2; i++) {
+    this.players[i % 2].addCard(self.currentGame.nextCard());
+  }
 }
 
 function hand() {
-
-  this.hit = function() {};
-  this.stand = function() {};
+  this.cards = [];
   this.score = {};
+  this.addCard = function(card) {
+    this.cards.push(card);
+  };
 }
