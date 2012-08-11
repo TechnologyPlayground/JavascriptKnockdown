055f5a94de60e909b82056a748ea3131038839a4
Andrew Benz <andorbal@gmail.com>
2012-08-11 15:45:33 -0500

    Created a set up and initial round test.

--- src/test/client/spec/RoundSpec.js
+++ src/test/client/spec/RoundSpec.js
@@ -2,7 +2,17 @@ describe("Round", function() {
   var round;
 
   beforeEach(function() {
-    round = new Round();
+    var foo = function() {
+      var index = 0;
+      this.nextCard = function() {
+        return ["D2", "D3", "D4", "D5", "D6", "D7", "D8"][index++]
+      };
+    };
+    round = new Round(new foo());
   });
 
+  it("initial deal interleves cards", function() {
+    expect(round.players[0]().cards()).toEqual(["D2", "D4"]);
+    expect(round.players[1]().cards()).toEqual(["D3", "D5"]);
+  });
 });
